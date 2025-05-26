71ef5a69ad09e8f6d0d9395e1049becff0c0af26
Bonala Sriman Rao <srimanrao@Bonalas-MacBook-Air.local>
2025-05-26 14:18:31 +0530

    django modified settings.py

--- django-first/mysite/settings.py
+++ django-first/mysite/settings.py
@@ -14,11 +14,10 @@ from pathlib import Path
 import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 import dj_database_url
 
@@ -93,15 +92,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'servermoniter',
-        'USER':'postgres',
-        'PASSWORD':'Sriman1234*',
-        'HOST':'localhost'
-    }
-}
 
 
 # Password validation
